0ce7cbf49ac501d9a63e107b31d67def544f3723
dushimsam <samueldushimimana95@gmail.com>
2022-04-02 23:09:44 +0200

    d1000000 problem solved google code jam 2022

--- competitive-programming/google-codejam/d1000000.cpp
+++ competitive-programming/google-codejam/d1000000.cpp
@@ -1,2 +1,35 @@
 #include<bits/stdc++.h>
-using namespace
+using namespace std;
+
+
+int findLongestLine(vector<int> numbers) {
+    int counts = 0;
+    sort(numbers.begin(), numbers.end());
+    for (int i = 0;i < numbers.size();i++) {
+        if (numbers[i] > counts) counts++;
+    }
+    return counts;
+}
+
+int main() {
+
+    int tests;
+    cin >> tests;
+    for (int test_case = 1; test_case <= tests; test_case++) {
+        vector<int> numbers;
+        int n;
+        cin >> n;
+        for (int i = 1;i <= n;i++)
+        {
+            int currN;
+            cin >> currN;
+            numbers.push_back(currN);
+        }
+        cout << "Case #" << test_case << ": ";
+        cout << findLongestLine(numbers);
+        cout << endl;
+    }
+
+
+    return 0;
+}
