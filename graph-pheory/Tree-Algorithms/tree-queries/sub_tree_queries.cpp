054e81cea6ba76020b63e232ddcc4dd055ff95f0
dushimsam <samueldushimimana95@gmail.com>
2022-04-12 18:20:04 +0200

    fixing logical bugs in sub-tree-queries algorithm

--- graph-pheory/Tree-Algorithms/tree-queries/sub_tree_queries.cpp
+++ graph-pheory/Tree-Algorithms/tree-queries/sub_tree_queries.cpp
@@ -3,15 +3,21 @@ using namespace std;
 
 class Tree
 {
-    map<int, list<int>> adj;
+    map<int,list<int>> adj;
+   
     int n;
     vector<vector<int>> BI;
 
 public:
-    Tree(int n)
+    Tree(int n,vector<vector<int>> nodeVals)
     {
         this->n = n;
         BI.resize(3, vector<int>(n, 0));
+
+        for(int i=0;i<nodeVals.size();i++){
+            // Assigning the nodes to their corresponding values.
+            BI[2][nodeVals[i][0]] = nodeVals[i][1];
+        }
     }
     void addEdge(int w, int x)
     {
@@ -21,14 +27,14 @@ public:
     void DFS(int v)
     {
         BI[0][v] = v;
-        BI[2][v] = 1;
-        
+        BI[1][v] = 1;
+
         list<int>::iterator it;
 
         for (it = adj[v].begin(); it != adj[v].end(); it++)
         {
             DFS(*it);
-            BI[2][v] += BI[2][*it];
+            BI[1][v] += BI[1][*it];
         }
       
     }
@@ -44,16 +50,35 @@ public:
             cout << endl;
         }
     }
+
+    int findSubTreeSum(int node){
+        int subTreeSum = 0;
+
+        for(int i=node;i< (BI[1][node]+node) ;i++){
+            cout<<"\n"<<BI[2][i];
+                 subTreeSum += BI[2][i];
+        }
+           
+
+        return subTreeSum;
+    }
 };
 
 int main()
 {
     int n = 9;
-    Tree tree(n);
+
     vector<vector<int>> adj = {{1, 5}, {1, 4}, {1, 3}, {1, 2}, {2, 6}, {4, 7}, {4, 8}, {4, 9}};
+    vector<vector<int>> nodeValues = {{1,2},{2,3},{3,5},{4,3},{5,1},{6,4},{7,4},{8,3},{9,1}};
+   
+    Tree tree(n+1,nodeValues);
     for (int i = 0; i < adj.size(); i++)
         tree.addEdge(adj[i][0], adj[i][1]);
     tree.DFS(1);
     tree.printBI();
+    
+    int u = 4;
+
+    cout<<"\n\n\nSUBTREE SUM OF "<<u << " : "<< tree.findSubTreeSum(u);
     return 0;
 }
