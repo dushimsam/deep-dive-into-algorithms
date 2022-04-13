09a40988c80c1d303ce226226cd2fbf984784821
dushimsam <samueldushimimana95@gmail.com>
2022-04-13 20:33:53 +0200

    written the   Forld-fulkerson algorithm

---
+++ graph-pheory/flows-and-cuts/maximum_flow_ford_fulkerson.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/***
+ *                  FORD FULKERSON
+ * SUMMARY -> 
+ *        REPEAT UNTIL THERE'S NO PATH TO DEST  
+ *          CHECK IF THERE'S A PATH FROM THE SOURCE TO DEST
+ *             REDUCE THE EDGE WEIGHTS ON THAT PATH BY MIN_WEIGTH AMONG THEM.
+ *          
+ *                 
+ *                      APPROACH 
+
+int total_flow = 0;
+
+while(thereIsApth)
+   minWeight = minElementInPath()
+   total_flow += minWeight;
+   reduce_by_minWeight() on path
+   thereIsApth = isThereAPath(graph)
+   
+***/
+
+int main(){
+
+
+
+    return 0;
+}
