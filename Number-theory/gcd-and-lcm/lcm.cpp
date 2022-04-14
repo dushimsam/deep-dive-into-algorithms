b78d2a0f45fbbdf1d9b54b2faf860001bdfbf1e8
dushimsam <samueldushimimana95@gmail.com>
2022-04-14 23:33:34 +0200

    finding the LCM in O(logn) super amazing

--- Number-theory/gcd-and-lcm/lcm.cpp
+++ Number-theory/gcd-and-lcm/lcm.cpp
@@ -1,17 +1,34 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 /***
-             LCM 
+             LCM - LOWEST COMMON MULTIPLE.
 
+            - The lowest number that can be divided by given numbers. (a , b)
 
-
-
-
+            LCM(a,b) = a*b / GCD(a,b)
 ***/
-int main(){
 
 
 
+// Euclidian algorithm
+int findGCD(int a, int b)
+{
+    if (b == 0)
+        return a;
+    return findGCD(b, a % b);
+}
+
+int findLCM(int a, int b)
+{
+    return (a * b) / findGCD(a, b);
+}
+
+int main()
+{
+    int a = 24;
+    int b = 36;
+
+    cout << " LCM OF " << a << " AND " << b << " IS: " << findLCM(24, 36);
     return 0;
 }
