98bc1ac3964dcb9cd8061c8afa54e5bdf2dbff5f
dushimsam <samueldushimimana95@gmail.com>
2022-04-14 22:37:46 +0200

    is a prime number in O(sqrt(n))

---
+++ Number-theory/primes-and-factors/is_aprime.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/***
+                              PROGRAM TO CHECK IF A NUMBER IS A PRIME IN O(sqrt(n)) time.
+
+APPROACH
+   It turns out that if a number is not a prime number there will numbers a , b 
+   such that a*b=n and  a <= sqrt(n) and b <= sqrt(n)
+
+   so it means that if we find a number from 2 to sqrt(n) divisible by n
+   then that n is not a prime number.
+***/
+bool isPrime(int n)
+{
+    for (int x = 2; x * x <= n; x++)
+    {
+        if(n % x == 0) return false;
+    }
+    return true;
+}
+
+int main()
+{
+    int n = 12;
+    cout<<isPrime(n);
+    return 0;
+}
