a1eb91278d6b71392368ec3c9ecbfbbd034876ef
dushimsam <samueldushimimana95@gmail.com>
2022-04-17 00:16:07 +0200

    is prime

--- Number-theory/primes-and-factors/is_aprime.cpp
+++ Number-theory/primes-and-factors/is_aprime.cpp
@@ -5,7 +5,7 @@ using namespace std;
                               PROGRAM TO CHECK IF A NUMBER IS A PRIME IN O(sqrt(n)) time.
 
 APPROACH
-   It turns out that if a number is not a prime number there will numbers a , b 
+   It turns out that if a number is not a prime number there will numbers a , b
    such that a*b=n and  a <= sqrt(n) and b <= sqrt(n)
 
    so it means that if we find a number from 2 to sqrt(n) divisible by n
@@ -13,9 +13,12 @@ APPROACH
 ***/
 bool isPrime(int n)
 {
+    if (n < 2)
+        return false;
     for (int x = 2; x * x <= n; x++)
     {
-        if(n % x == 0) return false;
+        if (n % x == 0)
+            return false;
     }
     return true;
 }
@@ -23,6 +26,6 @@ bool isPrime(int n)
 int main()
 {
     int n = 12;
-    cout<<isPrime(n);
+    cout << isPrime(n);
     return 0;
 }
