14123ad47b6be33041b0717582203e53126528dc
dushimsam <samueldushimimana95@gmail.com>
2022-04-21 12:22:43 +0200

    Count max venom on snake's DNA problem

--- greedy-algorithms/basic-principles/tempCodeRunnerFile.cpp
+++ greedy-algorithms/basic-principles/tempCodeRunnerFile.cpp
@@ -1,2 +1,11 @@
-    //    for(int i = 0; i < elements.size();i++)
-    //       cout<<endl<<elements[i] << "\n";
+  // for (int i = 0;i < input.length();i++) {
+  //   if (input[i] == snake[curr + 1] && map[snake[curr]]->first > 0) {
+  //     minE = min(map[snake[curr]], minE);
+  //     curr++;
+  //     map[snake[curr]] += 1;
+  //   }
+  //   else if (i == snake[curr]) {
+  //     if (map[curr] < minE)
+  //       map[snake[curr]] += 1;
+  //   }
+  // }
