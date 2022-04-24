f0f7807c4e2ddf232200308c28d2f4f44f632d20
dushimsam <samueldushimimana95@gmail.com>
2022-04-24 13:36:25 +0200

    wrong-answer problem solved

--- competitive-programming/codeforces/random/wrong_answer.cpp
+++ competitive-programming/codeforces/random/wrong_answer.cpp
@@ -1,11 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int solve(int num, int k) {
 
+    for (int i = 0; i < k; i++) {
+        if (num % 10 == 0) {
+            num /= 10;
+        }
+        else {
+            num--;
+        }
+    }
+    return num;
+}
+int main() {
 
-int main(){
-
-
-
+    int num, k;
+    cin >> num;
+    cin >> k;
+    cout << solve(num, k);
     return 0;
 }
