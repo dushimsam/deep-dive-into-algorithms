265f7dd71ffe47cf91d2f0011d109be1b677db8b
dushimsam <samueldushimimana95@gmail.com>
2022-05-12 21:41:09 +0200

    selection sorting _ easy to undestand

---
+++ sorting/selection_sorting.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef vector<int> vi;
+
+void printArr(vi arr){
+    cout<<"\n";
+    for(int e:arr) cout<<e<<" ";
+}
+void selectionSort(vi arr)
+{
+    int n = arr.size();
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        int minEli = i;
+
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[minEli])
+                minEli = j;
+        }
+
+        int temp = arr[i];
+        arr[i] = arr[minEli];
+        arr[minEli] = temp;
+    }
+
+    printArr(arr);
+}
+
+int main()
+{
+    vi arr = {64, 25, 12, 22, 11};
+    selectionSort(arr);
+
+    return 0;
+}
