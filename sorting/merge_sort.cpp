a80383422f2c1df775d30db9dcb78b343f038a09
dushimsam <samueldushimimana95@gmail.com>
2022-05-14 21:26:54 +0200

    Merge sorting

---
+++ sorting/merge_sort.cpp
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef vector<int> vi;
+
+/***
+ * @author Dush Sam.
+ *
+ *                       MERGE SORTING
+ * TIME COMPLEIXTY:
+ *      WORST CASE: O(nlogn)
+ *      BEST CASE: O(nlogn)
+ *
+ *                        APPROACH
+ * 
+ * 1.     DIVIDE ARR INTO TWO PARTS (PARTITIONING- log(n))
+ * 2.     IMPLEMENT SORTING IN O(n) WHILE MERGING.
+ *
+ *                        INSIGHTS
+ * 1. MergeSort works on the principle that joining together the sorted subsets can be done in O(n) time.
+ ***/
+
+
+
+void merge(vi &arr, int low, int mid, int high)
+{
+    int n1 = (mid - low) + 1;
+    int n2 = high - mid;
+    vi LEFT(n1);
+    vi RIGHT(n2);
+
+    // Fill the elements of the left arr and the right arr
+    for (int i = 0; i < n1; i++)
+    {
+        LEFT[i] = arr[i + low];
+    }
+
+    for (int i = 0; i < n1; i++)
+    {
+        RIGHT[i] = arr[i + mid + 1];
+    }
+
+    int k = low;
+    int i = 0;
+    int j = 0;
+
+    while (i < n1 && j < n2)
+    {
+        if (arr[i] < arr[j])
+        {
+            arr[k] = LEFT[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = RIGHT[j];
+            j++;
+        }
+        k++;
+    }
+
+    // the possible left elements in the LEFT
+    while (i < n1)
+    {
+        arr[k] = LEFT[i];
+        i++;
+        k++;
+    }
+
+    while (j < n2)
+    {
+        arr[k] = RIGHT[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(vi &arr, int low, int high)
+{
+    if (low < high)
+    {
+        int mid = (low + high) / 2;
+        // Partition the elements on the left
+        mergeSort(arr, low, mid);
+        // Continue partitioning the elemets on the right
+        mergeSort(arr, mid + 1, high);
+
+        // merge the two partitioned parts
+        merge(arr,low,mid,high);
+    }
+}
+
+void printArr(vi arr)
+{
+    cout << "\n";
+    for (int i = 0; i < arr.size(); i++)
+        cout << arr[i] << " ";
+}
+
+int main()
+{
+    vi arr = {64, 25, 12, 22, 11};
+    int n = arr.size();
+
+    mergeSort(arr, 0, n - 1);
+    printArr(arr);
+
+    return 0;
+}
