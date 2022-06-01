43eca34ef6c6eb4a65aecec04d3fb17cdf91c3cc
dushimsam <dushsam100@gmail.com>
2022-06-01 15:53:54 +0200

    done

--- graph-pheory/Tree-Algorithms/tree-queries/is_symmetric_tree.cpp
+++ graph-pheory/Tree-Algorithms/tree-queries/is_symmetric_tree.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 struct TreeNode
@@ -11,55 +11,35 @@ struct TreeNode
     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
 };
 
-bool isMirror(vector<int> arr){
-    int left = 0;
-    int right = arr.size()-1;
+bool checkIsSymmetric(TreeNode *left, TreeNode *right)
+{
+    if (left == NULL || right == NULL)
+        return (left == right);
+    if (left->val != right->val)
+        return false;
 
-    while(left < right){
-        if(arr[left] != arr[right]) return false;
-        left++;
-        right--;
-    }
-    return true;
+    return checkIsSymmetric(left->left, right->right) && checkIsSymmetric(left->right, right->left);
 }
 
-bool isSymmetric(TreeNode * root){
-     
-     queue<TreeNode*> parts[2];
-     queue<TreeNode*>* currQu = &parts[0];
-     queue<TreeNode*>* nextQu = &parts[1];
-     
-     int level = 0;
-     
-     currQu -> push(root);
-     
-     vector<int> levelNodes;
-
-     while(!currQu -> empty()){
-     TreeNode* currNode = currQu->front();
-     currQu->pop();
-
-     if(currNode -> left != NULL) nextQu -> push(currNode -> left);
-     if(currNode -> right != NULL) nextQu -> push(currNode -> right);
-      
-     levelNodes.push_back(currNode -> val);
-     
-    if(currQu->empty()){
-        level++;
-        if(!isMirror(levelNodes)) return false;
-        
-        currQu = &parts[level % 2];
-        nextQu = &parts[(level+1) % 2];
-    }
-     }
-     
-    return true;
+bool isSymmetric(TreeNode *root)
+{
+    if (root == NULL)
+        return true;
+    return checkIsSymmetric(root->left, root->right);
 }
 
-int main(){
+int main()
+{
 
-vector<int> arr={3};
-cout<<isMirror(arr);
+    TreeNode *root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(2);
 
+    root->left->right = new TreeNode(3);
+    root->right->right = new TreeNode(3);
+
+    bool res = isSymmetric(root);
+
+    cout << res;
     return 0;
 }
--- graph-pheory/Tree-Algorithms/tree-queries/tempCodeRunnerFile.cpp
+++
@@ -1,1 +0,0 @@
-        cout<<returned_left_K<<endl;
