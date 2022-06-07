a3fba2a7eac256646d47fa6762a19ea9e0671460
dushimsam <dushsam100@gmail.com>
2022-06-07 14:02:05 +0200

    linked lists detect cycle

--- interview-prep/arrays/two-pointers/linkedlists/detect_cycle.cpp
+++ interview-prep/arrays/two-pointers/linkedlists/detect_cycle.cpp
@@ -1,52 +1,59 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-struct ListNode{
+struct ListNode
+{
     int val;
-    ListNode* next = NULL;
-    ListNode(int val):val(val) {}
-    ListNode(int val,ListNode* next):val(val),next(next){}
+    ListNode *next = NULL;
+    ListNode(int val) : val(val) {}
+    ListNode(int val, ListNode *next) : val(val), next(next) {}
 };
-
 
 // METHOD 1 USING SET , HIGH SPACE COMPLEXITY
 
-// bool hasCycle(ListNode* head){
-//      set<int> visited;
+bool hasCycle(ListNode *head)
+{
+    set<ListNode *> visited;
 
-//      while(head != NULL){
-//          if(visited.find(visited.begin(),visited.end(),head->val)) return true;
+    while (head != NULL)
+    {
+        if (visited.find(head) != visited.end())
+            return true;
 
-//          visited.insert(head->val);
-//          head = head -> next;
-//      } 
+        visited.insert(head);
+        head = head->next;
+    }
 
-//      return false;
-// }
-
-// METHOD 2 ,  REDUCED SPACE COMPLEXITY
-bool hasCycleImproved(ListNode* head){
-     ListNode* walker = head;
-     ListNode* runner = head;
-
-     while(walker -> next != NULL && runner -> next -> next != NULL){
-         walker = walker -> next;
-         runner = runner-> next -> next;
-
-         if(walker == runner) return true;
-     }
-
-     return false;
+    return false;
 }
 
-int main(){
+// METHOD 2 ,  REDUCED SPACE COMPLEXITY
+bool hasCycleImproved(ListNode *head)
+{
+    ListNode *walker = head;
+    ListNode *runner = head;
 
-    ListNode* head = new ListNode(23);
-    head -> next = new ListNode(90);
-    head -> next -> next = new ListNode(100);
-    head -> next -> next -> next = new ListNode(90);
+    while (walker && runner && runner->next)
+    {
+        walker = walker->next;
+        runner = runner->next->next;
 
-    cout<< hasCycleImproved(head);
-    
+        if (walker == runner)
+            return true;
+    }
+
+    return false;
+}
+
+int main()
+{
+
+    ListNode *head = new ListNode(23);
+    head->next = new ListNode(90);
+    head->next->next = new ListNode(100);
+    head->next->next->next = new ListNode(90);
+
+    cout << hasCycleImproved(head);
+
     return 0;
 }
