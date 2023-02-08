a536ba45ed43efc7a3d626590a2c5b1f8a7e122f
dushimsam <dushsam@gmail.com>
2023-02-08 09:07:13 +0200

    solve(largest range): AlgoExpert

---
+++ algo-expert-premium/Arrays/largest_range.cpp
@@ -0,0 +1,56 @@
+#include <vector>
+typedef vector<int> vi;
+using namespace std;
+
+vector<int> largestRange(vector<int> array)
+{
+
+  int maxRange = 0;
+  vi res(2, 0);
+
+  unordered_map<int, bool> visited;
+
+  for (int n : array)
+  {
+    visited[n] = false;
+  }
+
+  for (int n : array)
+  {
+    if (visited[n])
+      continue;
+
+    int countLen = 1;
+    int left = n - 1;
+    int right = n + 1;
+    while (visited.find(left) != visited.end())
+    {
+      countLen++;
+      visited[left] = true;
+      left--;
+    }
+
+    while (visited.find(right) != visited.end())
+    {
+      countLen++;
+      visited[right] = true;
+      right++;
+    }
+    if (countLen > maxRange)
+    {
+      maxRange = countLen;
+      res[0] = left + 1;
+      res[1] = right - 1;
+    }
+  }
+  return res;
+}
+
+int main()
+{
+  vector<int> arr = {1, 11, 3, 0, 15, 5, 2, 4, 10, 7, 12, 6};
+  vector<int> res = largestRange(arr);
+  cout << res[1] << endl;
+
+  return 0;
+}
