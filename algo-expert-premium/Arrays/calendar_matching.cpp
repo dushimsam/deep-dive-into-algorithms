ead0e0bb726103f18cd89599f2d84191777aa4a9
dushimsam <dushsam@gmail.com>
2023-03-07 17:59:36 +0200

    feat(challenge-algoexpert): calendar matching problem

--- algo-expert-premium/Arrays/calendar_matching.cpp
+++ algo-expert-premium/Arrays/calendar_matching.cpp
@@ -1,5 +1,4 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
 struct StringMeeting
@@ -8,80 +7,127 @@ struct StringMeeting
     string end;
 };
 
-typedef vector<StringMeeting *> vs;
-typedef vector<vector<StringMeeting *>> vss;
+struct Meeting
+{
+    int start;
+    int end;
+};
 
 int timeToMinutes(string time)
 {
-    int position = time.find(":");
-    int minutes = stoi(time.substr(0, position));
-    int hours = stoi(time.substr(position + 1, time.length()));
-    return minutes + (hours * 60);
+    int delimiterPos = time.find(":");
+    int hours = stoi(time.substr(0, delimiterPos));
+    int min = stoi(time.substr(delimiterPos + 1, time.length()));
+
+    return (hours * 60) + min;
 }
 
-string minutesToTime(int minutes)
+string minutesToTime(int min)
 {
-    int hours = minutes / 60;
-    int minutes = minutes % 60;
+    int hours = min / 60;
+    int mins = min % 60;
+    string strHours = to_string(hours);
+    string strMins = mins < 10 ? "0" + to_string(mins) : to_string(mins);
 
-    return "" + hours == 0 ? "00" : to_string(hours) + ":" + to_string(minutes);
+    return strHours + ":" + strMins;
 }
 
-StringMeeting *extractBreak(string start, string end, int duration)
+vector<Meeting> updateCalendar(vector<StringMeeting> calendar, StringMeeting dailyBounds)
 {
-    int diffMin = timeToMinutes(end) - timeToMinutes(start);
-    StringMeeting *strMeeting = new StringMeeting();
+    vector<StringMeeting> updatedCalendar;
+    updatedCalendar.push_back({"0:00", dailyBounds.start});
+    updatedCalendar.insert(updatedCalendar.end(), calendar.begin(), calendar.end());
+    updatedCalendar.push_back({dailyBounds.end, "23:59"});
 
-    if (diffMin >= duration)
+    vector<Meeting> calendarInMinutes;
+    for (int i = 0; i < updatedCalendar.size(); i++)
     {
-        strMeeting->start = start;
-        strMeeting->end = minutesToTime(timeToMinutes(start) + duration);
-    }
-    else
-    {
-        strMeeting = nullptr;
+        calendarInMinutes.push_back({timeToMinutes(updatedCalendar[i].start), timeToMinutes(updatedCalendar[i].end)});
     }
 
-    return strMeeting;
+    return calendarInMinutes;
 }
 
-void extractBreaks(vector<StringMeeting> calendar1,
-                   StringMeeting dailyBounds1,
-                   vector<StringMeeting> calendar2,
-                   StringMeeting dailyBounds2,
-                   int meetingDuration, vs &breaks1, vs &breaks2)
+vector<Meeting> mergeCalendars(vector<Meeting> calendar1, vector<Meeting> calendar2)
 {
+    vector<Meeting> mergedCalendars;
+    // Merge the two calendar according to their starting time.
+    int i = 0;
+    int j = 0;
 
-    StringMeeting *strMeeting = extractBreak(dailyBounds1.start, calendar1[0].start, meetingDuration);
-    if (strMeeting != nullptr)
+    while (i < calendar1.size() && j < calendar2.size())
     {
-        breaks1.push_back(strMeeting);
-    }
-
-    strMeeting = extractBreak(dailyBounds2.start, calendar2[0].start, meetingDuration);
-    if (strMeeting != nullptr)
-    {
-        breaks2.push_back(strMeeting);
-    }
-
-    for (int i = 0; i < (calendar1.size() - 1); i++)
-    {
-        strMeeting = extractBreak(calendar1[i].end, calendar1[i + 1].start, meetingDuration);
-        if (strMeeting != nullptr)
+        if (calendar1[i].start < calendar2[j].start)
         {
-            breaks1.push_back(strMeeting);
+            mergedCalendars.push_back(calendar1[i]);
+            i++;
+        }
+        else
+        {
+            mergedCalendars.push_back(calendar2[j]);
+            j++;
         }
     }
 
-    for (int i = 0; i < (calendar2.size() - 1); i++)
+    while (i < calendar1.size())
     {
-        strMeeting = extractBreak(calendar2[i].end, calendar2[i + 1].start, meetingDuration);
-        if (strMeeting != nullptr)
+        mergedCalendars.push_back(calendar1[i++]);
+    }
+    while (j < calendar2.size())
+    {
+        mergedCalendars.push_back(calendar2[j++]);
+    }
+
+    return mergedCalendars;
+}
+
+vector<Meeting> flattenCalendar(vector<Meeting> calendar)
+{
+
+    vector<Meeting> flattened;
+    flattened.push_back(calendar[0]);
+    int currEnd = calendar[0].end;
+
+    for (int i = 1; i < calendar.size(); i++)
+    {
+        if (calendar[i].end < currEnd)
+            continue;
+        if (calendar[i].start < currEnd)
         {
-            breaks2.push_back(strMeeting);
+            currEnd = calendar[i].end;
+        }
+        else
+        {
+            flattened[flattened.size() - 1].end = currEnd;
+            flattened.push_back(calendar[i]);
+            currEnd = calendar[i].end;
         }
     }
+    return flattened;
 }
+
+vector<StringMeeting> getMatchingBreaks(vector<Meeting> meetings, int duration)
+{
+    vector<Meeting> matchingBreaks;
+
+    for (int i = 1; i < meetings.size(); i++)
+    {
+        if ((meetings[i].start - meetings[i - 1].end) >= duration)
+        {
+            matchingBreaks.push_back({meetings[i - 1].end, meetings[i].start});
+        }
+    }
+
+    vector<StringMeeting> updatedMeetings;
+
+    // convert into strings
+    for (Meeting meeting : matchingBreaks)
+    {
+        updatedMeetings.push_back({minutesToTime(meeting.start), minutesToTime(meeting.end)});
+    }
+    return updatedMeetings;
+}
+
 vector<StringMeeting> calendarMatching(vector<StringMeeting> calendar1,
                                        StringMeeting dailyBounds1,
                                        vector<StringMeeting> calendar2,
@@ -89,44 +135,55 @@ vector<StringMeeting> calendarMatching(vector<StringMeeting> calendar1,
                                        int meetingDuration)
 {
 
-    vs breaks1;
-    vs breaks2;
-    extractBreaks(calendar1,
-                  dailyBounds1,
-                  calendar2,
-                  dailyBounds2,
-                  meetingDuration, breaks1, breaks2);
+    // get updated calendar1
+    vector<Meeting> updatedCalendar1 = updateCalendar(calendar1, dailyBounds1);
+    // get updated calendar2
+    vector<Meeting> updatedCalendar2 = updateCalendar(calendar2, dailyBounds2);
+    // Merge Calendars
+    vector<Meeting> mergedCalendars = mergeCalendars(updatedCalendar1, updatedCalendar2);
+    // flatten calendars
+    vector<Meeting> flattenedMeetings = flattenCalendar(mergedCalendars);
+    vector<StringMeeting> breaks = getMatchingBreaks(flattenedMeetings, meetingDuration);
 
-    int i = 0;
-    int j = 0;
-
-    while(i < breaks1.size() && j < breaks2.size()){
-
-       string start_time;
-
-       int diffMin = timeToMinutes(breaks1[i].start) - timeToMinutes(breaks2[j].start);
-       
-       if(diffMin < 0){
-              start_time = breaks2[j].start;
-              bool notFound = true;
-
-              while(notFound){
-                int diff = timeToMinutes(start_time) - timeToMinutes(breaks2[j].end)
-                if(abs(diff) >= diff){
-                   
-                }
-              }
-              
-       }
-          
-        else start_time = breaks1[i].start;
-       
-
-    }
+    return breaks;
 }
 
 int main()
 {
 
+    // CALENDAR MATCHING PROBLEM
+
+    vector<StringMeeting> calendar1 = {
+        {"9:00", "10:30"},
+        {"12:00", "13:00"},
+        {"16:00", "18:00"}};
+
+    StringMeeting dailyBounds1 = {"9:00", "20:00"};
+
+    vector<StringMeeting> calendar2 = {
+        {"10:00", "11:30"},
+        {"12:30", "14:30"},
+        {"14:30", "15:00"},
+        {"16:00", "17:00"}};
+
+    StringMeeting dailyBounds2 = {"10:00", "18:30"};
+
+    int meetingDuration = 30;
+
+    // Expected result
+    /***
+     *
+     * 11:30-12:00
+     * 15:00-16:00
+     * 18:00-18:30
+     *
+     * ***/
+
+    vector<StringMeeting> result = calendarMatching(calendar1, dailyBounds1, calendar2, dailyBounds2, meetingDuration);
+
+    for (StringMeeting meeting : result)
+    {
+        cout << meeting.start << "-" << meeting.end << endl;
+    }
     return 0;
 }
