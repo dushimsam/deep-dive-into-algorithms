7e677368b550bfc06d68356f7bf4313da5610283
dushimsam <dushsam@gmail.com>
2023-06-27 20:28:46 +0200

    challenge(CSES-intro): weird algorithm

---
+++ cses-problemset/introductory-problems/weird_algorithm.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){
+    long long n;
+    cin >> n;
+
+    while(n != 1){
+      cout <<n<< " ";
+      if(n % 2 == 0){
+        n /= 2;
+      }else{
+        n = (n * 3) + 1;
+      }
+    }   
+    cout <<n<< " ";
+    return 0;
+}
