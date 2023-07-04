03a3dc6b85b62e28e30be7ada262ed01cf0da89f
dushimsam <dushsam@gmail.com>
2023-07-04 08:09:14 +0200

    challenge(CSES-intro): chessboards and queens

---
+++ cses-problemset/introductory-problems/chessboards_and_queens.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// void fill(vector<int> &prev, vector<string> s){
+//     for(int i = 0; i < prev.size(); i++){
+//         if(s[i])
+//     }
+// }
+
+
+int main(){
+   
+   int N = 8;
+   vector<string> s(8);
+   for(int i = 0; i < N; i++){
+      cin >> s[i];
+   } 
+
+
+    return 0;
+}
