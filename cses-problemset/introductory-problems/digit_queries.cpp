ffbf6330184aa7028e839b3e040a0b7bb16f4850
dushimsam <dushsam@gmail.com>
2023-07-13 08:14:04 +0200

    challenge(CSES-intro): digit queries

--- cses-problemset/introductory-problems/digit_queries.cpp
+++ cses-problemset/introductory-problems/digit_queries.cpp
@@ -3,32 +3,35 @@ using namespace std;
 typedef long long ll;
 
 ll pow10(ll x){
-     ll res = 1;
-    for(int i = 0; i < x; i++)
-        res *= 10;
-    return res;
+  ll res = 1;
+  
+  for(int i = 1; i <= x; i++){
+    res *=10;
+  }
+  return res;
 }
 
 ll solve(ll K){
-   if(K < 9)
-     return (int) K + 1;
+  if(K < 9)
+    return K + 1;
 
-   ll len = 1;
-   while(9 * pow10(len-1) * len < K){
-     K -= 9 * pow10(len-1) * len;
-     len++;
-   }
-   string S = to_string(pow10(len-1) + K/len);
+  ll len = 1;
+  while((len * 9 * pow10(len-1)) < K){
+    K -= len * 9 * pow10(len-1);
+    len++;
+  }
 
-   return int(S[K % len] - '0');
+  string S = to_string(pow10(len -1) + K/len);
+  return int(S[K % len] - '0');
 }
 
+
 int main(){
-    ll N, K;
-    cin >> N;
-    while(N--){
-     cin >> K;
-     cout << solve(K-1) <<"\n";
-    }
+   ll N, K;
+   cin >> N;
+   while(N--){
+    cin >> K;
+    cout << solve(K-1)<<"\n";
+   }
     return 0;
 }
